21d386c5730cf64d81c1222cca53d653a53c3649
rikitopus <ricardorkto@hotmail.com>
2026-04-19 23:51:14 -0300

    Criação do componente para exibição do recado

---
+++ src/components/recado.php
@@ -0,0 +1,6 @@
+<div class="recado">
+    <p><strong>Nome:</strong> <?php echo $nome; ?></p>
+    <p><strong>Mensagem:</strong> <?php echo $mensagem; ?></p>
+    <p><small><strong>Data:</strong> <?php echo $data; ?></small></p>
+</div>
+<hr>
