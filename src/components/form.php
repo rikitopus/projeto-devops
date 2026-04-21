52fcb0fa1035f6de91b98b356f0fb96a80620ec8
rikitopus <ricardorkto@hotmail.com>
2026-04-21 18:54:27 -0300

    Campo de data

--- src/components/form.php
+++ src/components/form.php
@@ -5,6 +5,8 @@
 <form action="" method="post">
     <label for="nome">Nome:</label>
     <input type="text" id="nome" name="nome">
+    <label for="data">Data:</label>
+    <input type="date" id="data" name="data" value="<?php echo date('Y-m-d'); ?>">
     <label for="mensagem">Mensagem:</label>
     <textarea id="mensagem" name="mensagem"></textarea>
     <button type="submit">Enviar</button>
